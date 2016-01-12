e2aff54490507aa04b32405b9840e09e1eb21d72
sergeimoiseev <moiseev.sergei@gmail.com>
2016-01-12 16:13:57 +0300

    first working test site

--- README.txt
+++ README.txt
@@ -8,3 +8,7 @@ C:\Users\Sergei\Projects\repetitor\README_COPY_FROM_EGRIXCALC.txt
 сервер на амазоне с пользователем repetitor и т.д.
 
 заработало 2016_01_12 в 14:15
+
+python app.py
+локальный адрес
+http://127.0.0.1:5000/
--- app.py
+++ app.py
@@ -3,12 +3,27 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
+question_0 = u"Реши задачу:\n2+2="
+
+q_list = []
+with open('quests.txt','rb') as q_file:
+    # 'quests.txt'
+    fullstr = q_file.read().decode('utf-8')
+q_list = fullstr.split('###')
+# q_list = [
+#             u"Паровоз идет прямо. Куда идет паровоз?",
+#             u"Паровоз идет влево. Куда идет паровоз?",
+#             u"Паровоз идет назад. Куда идет паровоз?",
+#             u"Паровоз идет вправо. Куда идет паровоз?",
+#             ]
+q_iter = iter(q_list)
+current_quest = ""
 # configuration
 DATABASE = 'app.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+USERNAME = 'valera'
+PASSWORD = 'valera'
 
 # create our little application
 app = Flask(__name__)
@@ -43,12 +58,21 @@ def show_entries():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    global current_quest, q_iter
     if not session.get('logged_in'):
         abort(401)
     g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+                 [current_quest, request.form['text']])
+                 # [request.form['title']+"\n2+2=", request.form['text']+"\n4"])
     g.db.commit()
-    flash('New entry was successfully posted')
+    try:
+        current_quest = next(q_iter)
+    except:
+        flash(u"Тест завершен. Для повторного прохождения войди заново.")
+        q_iter = iter(q_list)
+        return redirect(url_for('login'))
+        
+    flash(current_quest)
     return redirect(url_for('show_entries'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -61,7 +85,9 @@ def login():
             error = 'Invalid password'
         else:
             session['logged_in'] = True
-            flash('You were logged in')
+            global current_quest, q_iter
+            current_quest = next(q_iter)
+            flash(current_quest)
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
